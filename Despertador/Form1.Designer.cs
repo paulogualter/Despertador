5558b8ec2cc3b8ff58f6218ca1027b8f9ac21d6f
paulogualter <paulogualter@ig.com.br>
2015-08-14 13:51:07 -0300

    Aplicação Forms

--- Despertador/Form1.Designer.cs
+++ Despertador/Form1.Designer.cs
@@ -29,11 +29,251 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menuRestaurar = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuSobre = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.menuSair = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblresultado = new System.Windows.Forms.Label();
+            this.lblresultadoOk = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblhorario = new System.Windows.Forms.Label();
+            this.contextMenuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Ponto 1.0";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.BalloonTipClicked += new System.EventHandler(this.Form1_Resize);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuRestaurar,
+            this.menuSobre,
+            this.toolStripSeparator1,
+            this.menuSair});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(124, 76);
+            // 
+            // menuRestaurar
+            // 
+            this.menuRestaurar.Name = "menuRestaurar";
+            this.menuRestaurar.Size = new System.Drawing.Size(123, 22);
+            this.menuRestaurar.Text = "Restaurar";
+            this.menuRestaurar.Click += new System.EventHandler(this.menuRestaurar_Click);
+            // 
+            // menuSobre
+            // 
+            this.menuSobre.Name = "menuSobre";
+            this.menuSobre.Size = new System.Drawing.Size(123, 22);
+            this.menuSobre.Text = "Sobre";
+            this.menuSobre.Click += new System.EventHandler(this.menuSobre_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(120, 6);
+            // 
+            // menuSair
+            // 
+            this.menuSair.Name = "menuSair";
+            this.menuSair.Size = new System.Drawing.Size(123, 22);
+            this.menuSair.Text = "Sair";
+            this.menuSair.Click += new System.EventHandler(this.menuSair_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(23, 9);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(197, 250);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Horarios de Ponto";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Enabled = false;
+            this.textBox4.Location = new System.Drawing.Point(73, 198);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Enabled = false;
+            this.textBox3.Location = new System.Drawing.Point(73, 147);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(73, 94);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(73, 44);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 202);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "4° Ponto";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "3° Ponto";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "2° Ponto";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1° Ponto";
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::Despertador.Properties.Resources.green_ok_icon;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.Location = new System.Drawing.Point(249, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 161);
+            this.button1.TabIndex = 1;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblresultado
+            // 
+            this.lblresultado.AutoSize = true;
+            this.lblresultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblresultado.ForeColor = System.Drawing.Color.Red;
+            this.lblresultado.Location = new System.Drawing.Point(250, 189);
+            this.lblresultado.Name = "lblresultado";
+            this.lblresultado.Size = new System.Drawing.Size(0, 13);
+            this.lblresultado.TabIndex = 2;
+            // 
+            // lblresultadoOk
+            // 
+            this.lblresultadoOk.AutoSize = true;
+            this.lblresultadoOk.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblresultadoOk.ForeColor = System.Drawing.Color.Green;
+            this.lblresultadoOk.Location = new System.Drawing.Point(249, 222);
+            this.lblresultadoOk.Name = "lblresultadoOk";
+            this.lblresultadoOk.Size = new System.Drawing.Size(0, 13);
+            this.lblresultadoOk.TabIndex = 3;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblhorario
+            // 
+            this.lblhorario.AutoSize = true;
+            this.lblhorario.Location = new System.Drawing.Point(239, 259);
+            this.lblhorario.Name = "lblhorario";
+            this.lblhorario.Size = new System.Drawing.Size(0, 13);
+            this.lblhorario.TabIndex = 4;
+            this.lblhorario.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(420, 277);
+            this.Controls.Add(this.lblhorario);
+            this.Controls.Add(this.lblresultadoOk);
+            this.Controls.Add(this.lblresultado);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ponto 1.0";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblresultado;
+        private System.Windows.Forms.Label lblresultadoOk;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuRestaurar;
+        private System.Windows.Forms.ToolStripMenuItem menuSobre;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem menuSair;
+        private System.Windows.Forms.Label lblhorario;
     }
 }
 
--- Despertador/Form1.cs
+++ Despertador/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -16,5 +17,115 @@ namespace Despertador
         {
             InitializeComponent();
         }
+        string inicioExpediente = "Horsssario dass Manha";
+        string almoco = "Horario da Manha";
+        string voltaAlmoco = "Horario da Manha";
+        string fimExpediente = "Horario da Manha";
+
+        #region Comportamento
+        private void msgAlerta()
+        {
+            notifyIcon1.BalloonTipTitle = "Alerta de Ponto";
+            notifyIcon1.BalloonTipText = "Esta na hora de baater o ponto";
+            notifyIcon1.ShowBalloonTip(15000);
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string horario = DateTime.Now.ToLongTimeString();
+            lblresultadoOk.Text = horario;
+
+            switch (horario)
+            {
+                case "08:10:00":
+                    {
+                        
+                        msgAlerta();
+                        SoundPlayer alertaSonoro = new SoundPlayer(@"C:\Users\paulo.gualter\Desktop\Despertador\Despertador\mensagem.mp3");
+                        alertaSonoro.Play();
+
+                        break;
+                    }
+                case "11:50:00":
+                    {
+
+                        msgAlerta();
+                        SoundPlayer alertaSonoro = new SoundPlayer();
+                        alertaSonoro.Play();
+
+                        break;
+                    }
+                case "12:48:00":
+                    {
+
+                        msgAlerta();
+                        SoundPlayer alertaSonoro = new SoundPlayer();
+                        alertaSonoro.Play();
+
+                        break;
+                    }
+                case "17:53:00":
+                    {
+
+                        msgAlerta();
+                        SoundPlayer alertaSonoro = new SoundPlayer(@"C:\Suporte\mensagem.wav");
+                        alertaSonoro.Play();
+
+                        break;
+                    }
+            }
+        }
+        #endregion
+
+        #region Menus
+
+        private void menuRestaurar_Click(object sender, EventArgs e)
+        {
+            notifyIcon1.Visible = false;
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void menuSobre_Click(object sender, EventArgs e)
+        {
+            frmsobre sobre = new frmsobre();
+            sobre.Show();
+        }
+
+        private void menuSair_Click(object sender, EventArgs e)
+        {
+            notifyIcon1.Dispose();
+            Application.Exit();
+        }
+
+        #endregion
+
+        #region Formulario
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                notifyIcon1.Visible = true;
+                this.Hide();
+            }
+            else
+            {
+                notifyIcon1.Visible = false;
+                this.Show();
+                this.WindowState = FormWindowState.Normal;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SoundPlayer alertaSonoro = new SoundPlayer();
+            alertaSonoro.Play();
+        }
+
+        #endregion
+
+
     }
 }
